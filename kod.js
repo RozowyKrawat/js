cf34b32d692396f529945ad5bc246420310b7b60
RozowyKrawat <134552580+RozowyKrawat@users.noreply.github.com>
2023-05-25 16:58:43 +0200

    Create kod.js

---
+++ kod.js
@@ -0,0 +1,12 @@
+//22.04.2023, 12:24
+// ==UserScript==
+// @name         Kto tu jest
+// @namespace    http://tampermonkey.net/
+// @version      0.3.0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://experimental.margonem.pl/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=margonem.pl
+// @grant        none
+// ==/UserScript==
+let tablica = []
